966bd50b0ed404e6cbbd291b67935e56211842af
Lawrence Leymarie <lawrenceleymarie@Lawrences-MacBook-Pro.local>
2024-03-26 18:29:31 -0700

    Fixes push format

---
+++ Twitter Challenge/src/components/tweet.js
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+
+export default function Tweet({
+  imgSource,
+  imgAlt,
+  tweetText,
+  username,
+  profileName,
+  date,
+  isVerified,
+}) {
+  const [shares, setShares] = useState(0);
+  const [likes, setLikes] = useState(0);
+  const [replies, setReplies] = useState(0);
+
+  // Function to increment the replies count
+  const handleReplyClick = () => setReplies((prevReplies) => prevReplies + 1);
+
+  // Function to increment the shares count
+  const handleShareClick = () => setShares((prevShares) => prevShares + 1);
+
+  // Function to increment the likes count
+  const handleLikeClick = () => setLikes((prevLikes) => prevLikes + 1);
+
+  return (
+    <div className="tweet">
+      <img src={imgSource} alt={imgAlt} />
+      <div className="content">
+        <div className="top">
+          <div className="username">
+            <h1>{profileName}</h1>
+            {isVerified && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="verify"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
+          </div>
+
+          <p className="gray">{username}</p>
+          <p className="gray">· {date}</p>
+        </div>
+        <p>{tweetText}</p>
+        <div className="interactions gray">
+          <div className="replies interaction" onClick={handleReplyClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+              />
+            </svg>
+            {replies}
+          </div>
+          <div className="interaction" onClick={handleShareClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            {shares}
+          </div>
+          <div className="interaction" onClick={handleLikeClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+            {likes}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
